0e7defdd172e925b472c75e1896b1fbb02a250e8
nakatsu746 <jilptfes746@gmail.com>
2023-10-09 16:35:34 +0900

    クラス別に分割

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,21 +1,10 @@
-import Phaser from 'phaser';
+import Phaser, { Scene } from 'phaser';
 
 export default class PlayScene extends Phaser.Scene {
-    private player!: Phaser.Physics.Arcade.Sprite;
-    private playerX: number = 100;
-    private jump: number = 1500;
-    private canAirJump: boolean = false;
-
-    private speed: number = 2;
-    private speedUp: number = 0.001;
-    private gravity: number = 5000;
-    private height: number = 100;
-
-    private platforms!: Phaser.Physics.Arcade.StaticGroup;
-
-    private staticObstacles!: Phaser.Physics.Arcade.Group;
-    private movingObstacles!: Phaser.Physics.Arcade.Group;
-
+    private param: gameParameter = new gameParameter();
+    private platforms!: Platform;
+    private player!: Player;
+    private obstacles!: Obstacle;
 
     constructor() {
         super('PlayScene');
@@ -30,117 +19,148 @@ export default class PlayScene extends Phaser.Scene {
 
     create() {
         // platforms
-        const platformHeight = this.sys.canvas.height - this.height;
-        this.platforms = this.physics.add.staticGroup({
-            key: 'platform',    
-            repeat: 20,
-            setOrigin: { x: 0, y: 0 },
-            setXY: { x: 0, y: platformHeight, stepX: 500}
-        })
+        this.platforms = new Platform(this, this.param);
 
         // player
-        this.player = this.physics.add.sprite(this.playerX, 450, 'player');
-        this.player.setCollideWorldBounds(true);
-        this.player.setGravityY(this.gravity);
-        this.playerJump();
+        this.player = new Player(this, this.param);
+        this.player.jump(this);
 
-        // static obstacles
-        this.staticObstacles = this.physics.add.group();
-        this.generateRandomStaticObstacle();
-
-        // moving obstacles
-        this.movingObstacles = this.physics.add.group();
-        this.generateRandomMovingObstacle();
+        // obstacles
+        this.obstacles = new Obstacle(this, this.param);
+        this.obstacles.generate(this, 'staticObstacle');
+        this.obstacles.generate(this, 'movingObstacle');
 
         // collider
-        this.physics.add.collider(this.player, this.platforms);
-        this.physics.add.collider(this.player, this.staticObstacles, () => {
+        this.physics.add.collider(this.player.get(), this.platforms.get());
+        this.physics.add.collider(this.player.get(), this.obstacles.get(), () => {
             // game over
             this.physics.pause();
-            this.player.setTint(0xff0000);
+            this.player.get().setTint(0xff0000);
         }, undefined, this);
-        this.physics.add.collider(this.player, this.movingObstacles, () => {
-            // game over
-            this.physics.pause();
-            this.player.setTint(0xff0000);
-        })
-    }
-
-    playerJump() {
-        this.input.on('pointerdown', () => {
-            if (this.player.body.touching.down) {
-                this.player.setVelocityY(-this.jump);
-                this.canAirJump = true;
-            } else if (this.canAirJump) {
-                this.player.setVelocityY(-this.jump);
-                this.canAirJump = false;
-            }
-        });
-    }
-
-    generateRandomStaticObstacle() {
-        const minDelay = 3000;
-        const maxDelay = 15000;
-        const randomDelay = Phaser.Math.Between(minDelay, maxDelay);
-
-        this.time.addEvent({
-            delay: randomDelay,
-            callback: () => {
-                this.createStaticObstacle();
-                this.generateRandomStaticObstacle();
-            }
-        });
-    }
-
-    createStaticObstacle() {
-        const obstacle = this.staticObstacles.create(0, 0, 'staticObstacle');
-        obstacle.setX(this.sys.canvas.width + obstacle.displayWidth / 2);
-        obstacle.setY(this.sys.canvas.height - this.height - obstacle.displayHeight / 2);
-        return obstacle;
-    }
-
-    generateRandomMovingObstacle() {
-        const minDelay = 3000;
-        const maxDelay = 15000;
-        const randomDelay = Phaser.Math.Between(minDelay, maxDelay);
-
-        this.time.addEvent({
-            delay: randomDelay,
-            callback: () => {
-                this.createMovingObstacle();
-                this.generateRandomMovingObstacle();
-            }
-        });
-    }
-
-    createMovingObstacle() {
-        const obstacle = this.movingObstacles.create(0, 0, 'movingObstacle');
-        obstacle.setX(this.sys.canvas.width + obstacle.displayWidth / 2);
-        obstacle.setY(this.sys.canvas.height - this.height - obstacle.displayHeight / 2);
-        obstacle.setVelocityX(-this.speed * 60);
-        return obstacle;
     }
 
     update() {
-        this.speed += this.speedUp;
+        this.param.speedUp();
+        this.platforms.moving(this.param);
+        this.obstacles.moving(this.param);
+    }
+}
 
-        this.platforms.children.iterate((child) => {
+class gameParameter {
+    private speed: number = 10;
+    private increaseSpeed: number = 0.001;
+    private gravity: number = 5000;
+    private height: number = 200;
+
+    constructor() {}
+
+    getSpeed() { return this.speed; }
+    getGravity() { return this.gravity; }
+    getHeight() { return this.height;}
+
+    speedUp() {
+        this.speed += this.increaseSpeed;
+    }
+}
+
+class Platform {
+    private main!: Phaser.Physics.Arcade.StaticGroup;
+    private repeat: number = 20;
+    private step: number = 500;
+
+    get() { return this.main; }
+
+    constructor(scene: Phaser.Scene, param: gameParameter) {
+        const height = scene.sys.canvas.height - param.getHeight();
+        this.main = scene.physics.add.staticGroup({
+            key: 'platform',    
+            repeat: this.repeat,
+            setOrigin: { x: 0, y: 0 },
+            setXY: { x: 0, y: height, stepX: this.step}
+        })
+    }
+
+    moving(param: gameParameter) {
+        this.main.children.iterate((child) => {
             const platform = child as Phaser.Physics.Arcade.Image;
-            platform.x -= this.speed;
+            platform.x -= param.getSpeed();
 
             if (platform.x < -platform.displayWidth) {
                 platform.x = platform.scene.sys.canvas.width + platform.displayWidth;
             }
-        })
+        });
+    }
+}
 
-        this.staticObstacles.children.iterate((child) => {
+class Player {
+    private main!: Phaser.Physics.Arcade.Sprite;
+    private initPosX: number = 100;
+    private initPosY: number = 450;
+    private jumpPower: number = 1500;
+    private canAirJump: boolean = false;
+
+    constructor(scene: Phaser.Scene, param: gameParameter) {
+        this.main = scene.physics.add.sprite(this.initPosX, this.initPosY, 'player');
+        this.main.setGravityY(param.getGravity());
+    }
+
+    get() { return this.main;}
+
+    jump(scene: Phaser.Scene) {
+        scene.input.on('pointerdown', () => {
+            if (this.main.body.touching.down) {
+                this.main.setVelocityY(-this.jumpPower);
+                this.canAirJump = true;
+            } else if (this.canAirJump) {
+                this.main.setVelocityY(-this.jumpPower);
+                this.canAirJump = false;
+            }
+        });
+    }
+}
+
+class Obstacle {
+    private main!: Phaser.Physics.Arcade.Group;
+    private speed: number;
+    private height: number;
+    private minDelay: number = 3000;
+    private maxDelay: number = 15000;
+
+    constructor(scene: Phaser.Scene, param: gameParameter) {
+        this.main = scene.physics.add.group();
+        this.speed = param.getSpeed();
+        this.height = param.getHeight();
+    }
+
+    get() { return this.main; }
+
+    generate(scene: Phaser.Scene, type: string) {
+        const randomDelay = Phaser.Math.Between(this.minDelay, this.maxDelay);
+        scene.time.addEvent({
+            delay: randomDelay,
+            callback: () => {
+                this.create(scene, type);
+                this.generate(scene, type);
+            }
+        });
+    }
+
+    private create(scene: Phaser.Scene, type: string) {
+        const obstacle = this.main.create(0, 0, type);
+        obstacle.setX(scene.sys.canvas.width + obstacle.displayWidth / 2);
+        obstacle.setY(scene.sys.canvas.height - this.height - obstacle.displayHeight / 2);
+        return obstacle;
+    }
+
+    moving(param: gameParameter) {
+        this.get().children.iterate((child) => {
             if (!child) return;
             const obstacle = child as Phaser.Physics.Arcade.Image;
-            obstacle.x -= this.speed;
+            obstacle.x -= param.getSpeed();
 
             if (obstacle.x < -obstacle.displayWidth / 2) {
                 obstacle.destroy();
             }
-        })
+        });
     }
 }
