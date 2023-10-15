996cc914abd22fb5ae17174beaf66c1737cfceb6
nakatsu746 <jilptfes746@gmail.com>
2023-10-15 09:43:51 +0900

    デプロイ

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const config: Phaser.Types.Core.GameConfig = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false
+            debug: true
         }
     },
     scale: {
